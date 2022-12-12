c0a52535bb983ee0bb2492734dcccd6a08d087b9
Archit Wankhade <awankhade404@student.fuhsd.org>
2022-12-11 18:34:14 -0800

    changed to eval

--- days/day11.py
+++ days/day11.py
@@ -16,12 +16,10 @@ for line in inp.readlines():
 
 monkeys = []
 
-m = msg.split("\n\n")
-
-for monkey_txt in m:
+for monkey_txt in msg.split("\n\n"):
     m_l = monkey_txt.split("\n")
     items = list(map(int, re.findall(r'\d+', m_l[1])))
-    op = m_l[2].split("=")[1].strip()[3:].strip()
+    op = m_l[2].split(":")[1].strip()[6:].replace("old", "item")
     test = int(re.findall(r'\d+', m_l[3])[0])
     true_throw = int(re.findall(r'\d+', m_l[4])[0])
     false_throw = int(re.findall(r'\d+', m_l[5])[0])
@@ -29,24 +27,12 @@ for monkey_txt in m:
     monkeys.append(Monkey(0, items, op, test, true_throw, false_throw))
 
 def part1():
-    for round in range(20):
+    for _ in range(20):
         for monkey in monkeys:
             monkey.inspections += len(monkey.items)
             for item in monkey.items:
-                num = 0
-                if "old" in monkey.op:
-                    num = item
-                else:
-                    num = int(re.findall(r'\d+', monkey.op)[0])
-                
-                w_l = 0
-                if monkey.op[0] == '*':
-                    w_l = (item * num)//3
-                elif monkey.op[0] == "-":
-                    w_l = (item - num)//3
-                else:
-                    w_l = (item + num)//3
-                
+                w_l = eval(monkey.op) // 3
+
                 if w_l % monkey.test == 0:
                     monkeys[monkey.t_t].items.append(w_l)
                 else:
@@ -65,31 +51,22 @@ def part2():
     for monkey in monkeys:
         total_n *= monkey.test
 
-    for round in range(10000):
+    insp = [0] * len(monkeys)
+    for _ in range(10000):
+        i = 0
         for monkey in monkeys:
-            monkey.inspections += len(monkey.items)
+            insp[i] += len(monkey.items)
             for item in monkey.items:
-                num = 0
-                if "old" in monkey.op:
-                    num = item
-                else:
-                    num = int(re.findall(r'\d+', monkey.op)[0])
-                
-                w_l = 0
-                if monkey.op[0] == '*':
-                    w_l = (item * num) % total_n
-                elif monkey.op[0] == "-":
-                    w_l = (item - num) % total_n
-                else:
-                    w_l = (item + num) % total_n
+                w_l = eval(monkey.op) % total_n
 
                 if w_l % monkey.test == 0:
                     monkeys[monkey.t_t].items.append(w_l)
                 else:
                     monkeys[monkey.f_t].items.append(w_l)
             monkey.items = []
-                
-    insp = sorted([monkey.inspections for monkey in monkeys])
+            i += 1
+    insp.sort()
     print(insp[-1] * insp[-2])
 
 part2()
+
